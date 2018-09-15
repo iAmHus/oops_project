9bb018ffa1928d579e9a75f4c83da39e96ae1827
Huss <email.of.huss@gmail.com>
2018-09-15 19:58:33 -0400

    Moved things to proper folders

--- src/com/assignment_2/FindPrimeFactor.java
+++ src/com/assignment_2/FindPrimeFactor.java
@@ -1,64 +1,36 @@
-package com.assignment2;
+package com.assignment_2;
 
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * The class that has a static method 'findPrimeFactor' that finds the index of the prime-factor among the two lists.
+ *
+ * Contributions:
+ *      - Murtuza Hussain Shareef(G01024452)
+ *              - Implementation for the findPrimeFactor method
+ *              - Top-level Javadoc and for all the utility methods involved
+ *      - Harisha Rajam Swaminathan(G01085509)
+ *              - Suggested more boundary scenarios that the code should cater to
+ *              - JavaDoc for the findPrimeFactor method
+ *      - Shubham Pudi (G01087664)
+ *              Tested using Junit for different scenarios where
+ *                  - positive flows were covered
+ *                  - exceptions were thrown
+ *
+ *       In addition to this, every one contributed their ideas when collaborating and building the code together
+ */
+
+
 public class FindPrimeFactor {
-
-
-    //TODO: Do we need to throw the exceptions - that were given as comments in Assignment 1
-    //TODO: Do we need to create a html file for Javadoc.
-
-
-    //Sample code to test it out !
-    public static void main(String[] args) {
-
-        List<Integer> a = null;
-        List<Integer> b = null;
-//        System.out.println("Index of the prime factor is " + findPrimeFactor(a, b));
-
-        a = Arrays.asList(12, 4, 9);
-        b = Arrays.asList(1, -2, 3);
-//        System.out.println("Index of the prime factor is " + findPrimeFactor(a, b));
-
-        a = Arrays.asList(12, 4, 9);
-        b = Arrays.asList(1, 0, 3);
-//        System.out.println("Index of the prime factor is " + findPrimeFactor(a, b));
-
-        a = Arrays.asList(12, 4, 8);
-        b = Arrays.asList(1, 3, 3);
-//        System.out.println("Index of the prime factor is " + findPrimeFactor(a, b));
-    }
-
-
-    // Requires: a not null; b not null;
-    //           there is some index i where b[i] is
-    //           both prime and a factor of a[i]
-
-
-    //          NPE - handle case when individual elements are null
-    //          Arithmetic exception -  divide by 0 ; negative numbers ;
-
-
-    // Effects: return the least index
-    //          at which b[i] is a prime factor of a[i]
-    // E.g. findPrimeFactor ([12, 25, 18, 8], [6, 2, 3, 2]) = 2
-    // (Note: 6 is a factor of 12, but is not prime,
-    //  and 2 is prime, but is not a factor of 25.  However,
-    //  3 is a prime factor of 18. Hence, index "2" is the correct
-    //  answer.  index "3" is not a possible answer, because the
-    //  third index is not the least index with the desired property.)
-
-    // Also note that a[] and b[] need not be of the same length.
 
     /**
      * Find prime factor int.
      *
      * @param a the input list 'a'
      * @param b the input list 'b'
-     * @return the index at which b[i] is a prime-factor of a[i]
+     * @return the index 'i' at which b[i] is a prime-factor of a[i]
      * @throws NullPointerException     when either one of the inputs passed is a null list (NOT a list containing nulls)
-     * @throws IllegalArgumentException  when there is no element in 'b' which is a prime factor of the corresponding element in 'a'
+     * @throws IllegalArgumentException when there is no element in 'b' which is a prime factor of the corresponding element in 'a'
      */
     public static int findPrimeFactor(List<Integer> a, List<Integer> b) throws NullPointerException, IllegalArgumentException {
 
@@ -106,12 +78,26 @@ public class FindPrimeFactor {
         return indexOfPrimeFactor;
     }
 
-    private static boolean isFactorOf(int elementInB, int elementInA) {
-        return elementInA % elementInB == 0;
+    /**
+     * Utility method that checks if element1 is a factor of element2.
+     *
+     * @param element2 the element2
+     * @param element1 the element1
+     *
+     * @return the boolean value indicating if element2 is a factor of element1
+     */
+    private static boolean isFactorOf(int element2, int element1) {
+        return element1 % element2 == 0;
     }
 
 
-    public static boolean isPrime(int a) {
+    /**
+     * Utility method that checks if the element passed is a prime
+     *
+     * @param a the element to check
+     * @return the boolean check indicating if the number passed is a prime or not
+     */
+    private static boolean isPrime(int a) {
 
         if (a == 1) {
             return false;
