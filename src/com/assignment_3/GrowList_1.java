a39a424d58e6f8030e7f5a880edc30cb65202a80
Huss <email.of.huss@gmail.com>
2018-09-18 14:37:28 -0400

    Final main code

--- src/com/assignment_3/GrowList_1.java
+++ src/com/assignment_3/GrowList_1.java
@@ -1,16 +1,29 @@
 package com.assignment_3;
 
 import java.util.*;
-//  GrowList is a mutable list that only gets longer.
-public final class GrowList <E> {
+//  GrowList_1 is a mutable list that only gets longer.
 
-    private final Map<Integer,E> values;
+// GitKraken Sample commit
+public class GrowList_1<E> {
 
-    private GrowList() { values = new HashMap<Integer,E>();}
+    private Map<Integer,E> values;
+
+    // Changed the visibility of the constructor to private
+    private GrowList_1() { values = new HashMap<Integer,E>();}
 
     // add to the end of the list
-    public void add(E o) {
-        values.put(size(), o);
+
+    // Writing a producer method - that imposes the immutability behavior
+
+    public GrowList_1<E> add(E o) {
+
+        GrowList_1 result = new GrowList_1();
+        result.values.putAll(this.values);
+
+        result.values.put(size(), o);
+
+        return result;
+//        values.put(size(), o);
     }
 
     // number of values in list
@@ -18,14 +31,20 @@ public final class GrowList <E> {
 
     // get ith value in list
     public E get(int i) {
-        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.get");
+        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList_1.get");
         return values.get(i);
     }
 
     // update ith value in list;  return previous value or null if none
-    public E set(int i, E o) {
-        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList.set");
-        return values.put(i, o);
+    public GrowList_1<E> set(int i, E o) {
+        if (! inRange(i)) throw new IndexOutOfBoundsException("GrowList_1.set");
+
+        GrowList_1 result = new GrowList_1();
+        result.values.putAll(this.values);
+
+        result.values.put(i, o);
+
+        return result;
     }
 
     // private helper method
@@ -41,7 +60,7 @@ public final class GrowList <E> {
     }
 
     public static void main(String[] args) {
-        GrowList<String> list = new GrowList<String>();
+        GrowList_1<String> list = new GrowList_1<String>();
 
         System.out.println("list is:" + list);
         list.add("cat");
