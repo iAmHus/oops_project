c9c9d0e5bb0d25a655f26b9b698b411bbe598264
Huss <email.of.huss@gmail.com>
2018-10-16 15:27:59 -0400

    Final commit for assignment 6

--- src/com/assignment_6/BagTest.java
+++ src/com/assignment_6/BagTest.java
@@ -1,6 +1,7 @@
 package com.assignment_6;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class BagTest {
 
@@ -9,9 +10,32 @@ public class BagTest {
         Bag<String> stringBag = new Bag<>();
 
         stringBag.insert("cat");
-        stringBag.insert("cat");
+        stringBag.insert("dog");
+
+        assertTrue(stringBag.repOk());
+    }
+
+
+    @Test
+    public void testPropertiesRule() {
+        Bag<String> stringBag = new Bag<>();
+        LiskovGenericSet<String> stringSet = new LiskovGenericSet<>();
+
+        //Adding an element to LiskovGenericSet & Bag
+        stringSet.insert("bla-1");
+        stringBag.insert("bla-1");
+
+        //Adding the same element to LiskovGenericSet & Bag
+        stringSet.insert("bla-1");
+        stringBag.insert("bla-1");
+
+        assertEquals(stringBag.size(), stringSet.size());
+
 
 
 
     }
+
+
+
 }
