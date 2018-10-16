90d5aaceedc8edd68f60b9bfc156be129a21fecb
Huss <email.of.huss@gmail.com>
2018-10-16 15:47:39 -0400

    Final commit for assignment 6

--- src/com/assignment_6/BagTest.java
+++ src/com/assignment_6/BagTest.java
@@ -16,6 +16,17 @@ public class BagTest {
     }
 
 
+    /**
+     * Test properties rule.
+     *
+     * Validates the condition -
+     * (continuing from the discussion in 7.11 - Liskov) - Is Bag a legitimate subtype of Set ?
+     *
+     * This is a test case that FAILS !!
+     *                  - because the Bag allows duplicates - but LiskovGenericSet does NOT !
+     *                  - This causes violation of the properties-rule (i.e.)
+     *                          The super-type defines a property that is lost in its sub-type
+     */
     @Test
     public void testPropertiesRule() {
         Bag<String> stringBag = new Bag<>();
@@ -29,11 +40,13 @@ public class BagTest {
         stringSet.insert("bla-1");
         stringBag.insert("bla-1");
 
-        assertEquals(stringBag.size(), stringSet.size());
+        // Attempting to remove the element -  the bag as it allows duplicates - does NOT completely remove the element
+        //                                  -  while the set would completely removes the element
+        stringBag.remove("bla-1");
+        stringSet.remove("bla-1");
 
-
-
-
+        assertFalse(stringSet.isIn("bla-1"));
+        assertFalse(stringBag.isIn("bla-1"));
     }
 
 
