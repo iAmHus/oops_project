253dfa63a5d574ff8452e7778659d2d157fda5bc
shubham <pudi.subham@gmail.com>
2018-10-16 15:53:23 -0400

    Juint test cases for all the methods

--- src/com/assignment_6/BagTest.java
+++ src/com/assignment_6/BagTest.java
@@ -2,8 +2,13 @@ package com.assignment_6;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.*;
 public class BagTest {
 
+	Bag<String> b =new Bag<>();
     @Test
     public void repOk() {
         Bag<String> stringBag = new Bag<>();
@@ -13,5 +18,36 @@ public class BagTest {
 
 
 
+    }
+    @Test
+    public void testInsert()
+    {
+    	b.insert("Chandler");
+    	assertEquals("1",b.getValue("Chandler"));    	
+    }
+    @Test
+    public void testRemove()
+    {
+    	b.remove("Chandler");
+    	assertEquals("0",b.getValue("Chandler"));
+    }
+    
+    @Test
+    public void testisIn()
+    {
+        b.insert("Joey");
+    	assertTrue(b.isIn("Joey"));
+    }
+    @Test
+    public void testgetValue()
+    {
+    	
+    	b.insert("Chandler");
+    	assertEquals("1",b.getValue("Chandler"));
+    }
+    @Test
+    public void testSize()
+    {
+    	assertEquals("2",b.size());
     }
 }
